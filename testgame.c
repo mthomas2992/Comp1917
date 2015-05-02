bcafbb27527d5a45daee7a81596bad1c0905b271
Brymo <bryanmoh@yahoo.com.au>
2015-05-02 14:08:43 +1000

    Mah Tests!
    Added a few simple tests for my part.  Not to sure what dispose game does.  Will talk to Salil about it.

--- testgame.c
+++ testgame.c
@@ -12,61 +12,55 @@
 
 #include "Game.h"
 
-// Joerick's #defines
-#define MIN_DICE_VALUE 1
-#define MAX_DICE_VALUE 12
-	// Change MAX_DICE_VALUE to 6 if two dice are rolled
-
-
 int main (int argc, char *argv[]) {
 	//add your tests for your section in here, 
 	
 	
-	//Bryan's Tests!
+	//BRYAN'S TESTS!
 	
-	// Joerick's tests
-		// throwDice test
-		Game g = newGame(DEFAULT_DISCIPLINES,DICE_VALUE);
+	//disposeGame
+	printf("Testing disposeGame");
 	
-		assert(getWhoseTurn(g) == 0);
-		assert(getTurnNumber(g) == -1);
+	//Not too sure what this does...
 	
-		throwDice(g,DICE_VALUE[0]);
-		assert(DICE_VALUE[0] >= MIN_DICE_VALUE && DICE_VALUE[0] <= MAX_DICE_VALUE)
-			// Change MAX_DICE_VALUE definiton to 6 if two dice are rolled
-		assert(getWhoseTurn(g) == UNI_A);
-		assert(getTurnNumber(g) == 0);
-
-		throwDice(g,DICE_VALUE[1]);
-		assert(DICE_VALUE[1] >= MIN_DICE_VALUE && DICE_VALUE[1] <= MAX_DICE_VALUE)
-		assert(getWhoseTurn(g) == UNI_B);
-		assert(getTurnNumber(g) == 1);
-
-		throwDice(g,DICE_VALUE[2]);
-		assert(DICE_VALUE[2] >= MIN_DICE_VALUE && DICE_VALUE[2] <= MAX_DICE_VALUE)
-		assert(getWhoseTurn(g) == UNI_C);
-		assert(getTurnNumber(g) == 2);
-
-		throwDice(g,DICE_VALUE[3]);
-		assert(DICE_VALUE[3] >= MIN_DICE_VALUE && DICE_VALUE[3] <= MAX_DICE_VALUE)
-		assert(getWhoseTurn(g) == UNI_A);
-		assert(getTurnNumber(g) == 3);
+	printf("disposeGame passed!")
 	
-		throwDice(g,DICE_VALUE[4]);
-		assert(DICE_VALUE[3] >= MIN_DICE_VALUE && DICE_VALUE[4] <= MAX_DICE_VALUE)
-		assert(getWhoseTurn(g) == UNI_B);
-		assert(getTurnNumber(g) == 4);
-
-		throwDice(g,DICE_VALUE[5]);
-		assert(DICE_VALUE[5] >= MIN_DICE_VALUE && DICE_VALUE[5] <= MAX_DICE_VALUE)
-		assert(getWhoseTurn(g) == UNI_C);
-		assert(getTurnNumber(g) == 5);
-
-		throwDice(g,DICE_VALUE[6]);
-		assert(DICE_VALUE[6] >= MIN_DICE_VALUE && DICE_VALUE[6] <= MAX_DICE_VALUE)
-		assert(getWhoseTurn(g) == UNI_A);
-		assert(getTurnNumber(g) == 6);
-		// End of throwDice test
+	printf("Testing getDiceValue");
+	assert( getDiceValue (g, regionID) >= 2);
+	assert( getDiceValue (g, regionID) <= 12);
+	printf("getDiceValue passed!")
+	
+	printf("Testing getMostArcs");
+	//Arcs array will hold arcs per player
+	int arcs[3];
+	int arcCount = 1;
+	while(arcCount < 4){
+		assert(getMostPublications(g) >= arcs[arcCount]);
+		arcCount++;
+	}
+	printf("getMostArcs passed!")
+	
+	printf("Testing getMostPublications");
+	//Publications array will hold publications per player
+	int publications[3];
+	int pubCount = 1;
+	while(count < 4){
+		assert(getMostPublications(g) >= publications[pubCount]);
+		pubCount++;
+	}
+	printf("getMostPublications passed!")
+	
+	printf("Testing getTurnNumber");
+	newGame (int discipline[], int dice[]);
+	assert(getTurnNumber(g) == -1);
+	throwDice (g, diceScore);
+	assert(getTurnNumber(g) == 0);
+	printf("getTurnNumber passed!")
+	
+	
+	//Don't forget to...
+	
+	return EXIT_SUCCESS;
 	
 }
 
