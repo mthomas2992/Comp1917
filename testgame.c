2045bb2d1e5acd3e51e5d773f55b07391e375e0e
mthomas2992 <mthomas2992@gmail.com>
2015-05-03 15:37:23 +1000

    Add newGameTests, setup basis for all other tests
    still needs testing on the tests themselves...

--- testgame.c
+++ testgame.c
@@ -16,9 +16,68 @@
 #define MIN_DICE_VALUE 2
 #define MAX_DICE_VALUE 12
 
+//Standard defines
+
+#define NUM_UNIS 3
+
+// player ID of each university
+#define NO_ONE 0
+#define UNI_A 1
+#define UNI_B 2
+#define UNI_C 3
+
+// contents of an ARC
+#define VACANT_ARC 0
+#define ARC_A 1
+#define ARC_B 2
+#define ARC_C 3
+
+// contents of a VERTEX
+#define VACANT_VERTEX 0
+#define CAMPUS_A 1
+#define CAMPUS_B 2
+#define CAMPUS_C 3
+#define GO8_A 4
+#define GO8_B 5
+#define GO8_C 6
+
+// action codes
+#define PASS 0
+#define BUILD_CAMPUS 1
+#define BUILD_GO8 2
+#define OBTAIN_ARC 3
+#define START_SPINOFF 4
+#define OBTAIN_PUBLICATION 5
+#define OBTAIN_IP_PATENT 6
+#define RETRAIN_STUDENTS 7
+
+// disciplines
+#define STUDENT_THD 0
+#define STUDENT_BPS 1
+#define STUDENT_BQN 2
+#define STUDENT_MJ  3
+#define STUDENT_MTV 4
+#define STUDENT_MMONEY 5
+
+#define NUM_REGIONS 19
+#define PATH_LIMIT 150
+
+#define TRUE 1
+#define FALSE 0
+
+//New game test defaults
+#define DEFAULT_DISCIPLINES {STUDENT_BQN, STUDENT_MMONEY, STUDENT_MJ,
+                STUDENT_MMONEY, STUDENT_MJ, STUDENT_BPS, STUDENT_MTV,
+                STUDENT_MTV, STUDENT_BPS,STUDENT_MTV, STUDENT_BQN,
+                STUDENT_MJ, STUDENT_BQN, STUDENT_THD, STUDENT_MJ,
+                STUDENT_MMONEY, STUDENT_MTV, STUDENT_BQN, STUDENT_BPS }
+#define DEFAULT_DICE {9,10,8,12,6,5,3,11,3,11,4,6,4,7,9,2,8,10,5}
+
 void bryanstests();
 void Joerickstests();
-
+void makeActiontests();
+void getDisciplinetests();
+void newGametests();
 
 int main (int argc, char *argv[]) {
 	//add your tests for your section in here, 
@@ -26,6 +85,11 @@ int main (int argc, char *argv[]) {
 
    Joerickstests();
 
+   newGametests();
+
+   makeActiontests();
+
+   getDisciplinetests();
    
 	return EXIT_SUCCESS;
 	
@@ -167,6 +231,94 @@ void Joerickstests(){
    /*****************************************************/
 }
 
+void newGametests(){
+   int disciplines[] = DEFAULT_DISCIPLINES;
+   int dice[] = DEFAULT_DICE;
+   Game g =newGame (disciplines, dice);
+   
+   //ensure starting values are completley empty
+   assert(getMostARCs(g)==0);
+   assert(getMostPublications(g)==0);
+   assert(getTurnNumber(g)==-1); // first turn is identified as -1
+   assert(getWhoseTurn(g)==NO_ONE);
+
+   //check that each player is at the initial for all their values
+   int playerx=UNI_A;
+   int diciplinecheck=STUDENT_THD;
+   while (playerx<=UNI_C){
+      assert(getKPIpoints(g,playerx)==0);
+      assert(getARCs(g,playerx)==0);
+      assert(getGO8s(g,playerx)==0);
+      assert(getCampuses(g,playerx)==0);
+      assert(getIPs(g,playerx)==0):
+      assert(getPublications(g,playerx)==0);
+      while(diciplinecheck<=STUDENT_MMONEY){
+         if ((diciplinecheck==STUDENT_BPS) || (diciplinecheck==STUDENT_BQN)){
+            assert(getStudents(g,playerx,diciplinecheck)==3);
+         } else if (diciplinecheck==STUDENT_THD) {
+            assert(getStudents(g,playerx,diciplinecheck)==0);
+         } else{
+            assert(getStudents(g,playerx,diciplinecheck)==1);
+         }
+         diciplinecheck++
+      }
+      playerx++;
+      diciplinecheck==STUDENT_THD;
+   }
+
+   //Vertex generator/checker VERY VERY VERY DODGY ONLY CHECKS CONSTANT LEFTS
+   int pathcount=0;
+   path pathtest;  //unsure about syntax
+   while (pathcount<PATH_LIMIT){
+      pathtest[pathcount]='L';
+      assert(getCampus(g,pathtest)==VACANT_VERTEX);
+      pathcount++
+   }
+
+   //check exchange rate for all players
+   int diciplinecheck2=1;
+   int diciplinecheck3=1;
+   playerx=UNI_A;
+   while (playerx<=UNI_C){
+      while(diciplinecheck2<=STUDENT_MMONEY){
+         while(diciplinecheck3<=STUDENT_MMONEY){
+            assert(getExchangeRate(g,playerx,diciplinecheck2,diciplinecheck3)==3);
+            diciplinecheck3++;
+         }
+         diciplinecheck2++;
+         diciplinecheck3=1;
+      }
+   playerx++;
+   diciplinecheck2=1;
+   diciplinecheck3=1;
+   }
+
+}
+
+void makeActiontests(){
+
+   printf("testing makeAction\n");
+   Game g =newGame(DEFAULT_DISCIPLINES,DICE_VALUE);
+   
+   //test arc actions
+
+   int playeronecheck=0;
+   int playertwocheck=0;
+   int playerthreecheck=0;
+   int totalarcs=0;
+
+   playeronecheck=getARCs(g,UNI_A);
+   playertwocheck=getARCs(g,UNI_B);
+   playerthreecheck=getARCs(g,UNI_C);
+   totalarcs=playerthreecheck+playertwocheck+playeronecheck;
+
+   makeAction(g,OBTAIN_ARC);
+
+}
+
+void getDisciplinetests(){
+
+}
 
 /*
 Game newGame (int discipline[], int dice[]); //Matt
@@ -194,6 +346,7 @@ int getCampus(Game g, path pathToVertex); //Joerick
 int getARC(Game g, path pathToEdge); //Joerick
 
 int isLegalAction (Game g, action a); //Matt
+cannot go back on first arc road
 
 int getKPIpoints (Game g, int player); //Charly
 
