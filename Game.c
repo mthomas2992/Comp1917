e1eddeb05a7b1a49eba9d5a218117ef7240b9d74
mthomas2992 <mthomas2992@gmail.com>
2015-05-07 15:33:05 +1000

    Added newgame function

--- Game.c
+++ Game.c
@@ -44,6 +44,64 @@ typedef struct _game {
 int main (int argc, char *argv[]) {
 
     //I like commenting Get used to it.
+    //I don't actually know whats meant to go in our mainline really...
 
 	return EXIT_SUCCESS;
+}
+Game newGame (int discipline[], int dice[]){ //Matt
+	game g;
+   g.mostarcs=NO_ONE;
+   g.mostpubs=NO_ONE;
+   g.turncount=-1; //terra nullius
+   g.whoseTurn=NO_ONE; //terra nullius
+   
+   int i=0;
+   while(i<NUM_REGIONS){
+      g.regions[i]=discipline[i];
+      g.regionid[i]=dice[i];
+   }
+
+   //player one
+   g.player1.KPI=20;
+   g.player1.arcs=0;
+   g.player1.GO8s=0;
+   g.player1.Campuses=2;
+   g.player1.IPs=0;
+   g.player1.Pubs=0;
+   g.player1.students.THD=0;
+   g.player1.students.BPS=3;
+   g.player1.students.BQN=3;
+   g.player1.students.MJ=0;
+   g.player1.students.MTV=0;
+   g.player1.students.MMONEY=0;
+
+   //player two
+   g.player2.KPI=20;
+   g.player2.arcs=0;
+   g.player2.GO8s=0;
+   g.player2.Campuses=2;
+   g.player2.IPs=0;
+   g.player2.Pubs=0;
+   g.player2.students.THD=0;
+   g.player2.students.BPS=3;
+   g.player2.students.BQN=3;
+   g.player2.students.MJ=0;
+   g.player2.students.MTV=0;
+   g.player2.students.MMONEY=0;
+
+   //player three
+   g.player3.KPI=20;
+   g.player3.arcs=0;
+   g.player3.GO8s=0;
+   g.player3.Campuses=2;
+   g.player3.IPs=0;
+   g.player3.Pubs=0;
+   g.player3.students.THD=0;
+   g.player3.students.BPS=3;
+   g.player3.students.BQN=3;
+   g.player3.students.MJ=0;
+   g.player3.students.MTV=0;
+   g.player3.students.MMONEY=0;
+   
+   return g;
 }
